9adef47e58438016936e4d38bf5067bc769472a8
Ryan Lloyd <ryan.lloyd10@outlook.com>
2017-04-26 14:58:11 +0100

    Added Mock

---
+++ Mock Exam/Candidate/Holidays/Holidays/Holidays/Customers.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Customers
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtCustomerID = New System.Windows.Forms.TextBox()
+        Me.lblCustomerEmail = New System.Windows.Forms.Label()
+        Me.lblCustomerName = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblDOB = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtCustomerEmail = New System.Windows.Forms.TextBox()
+        Me.lblCustomerID = New System.Windows.Forms.Label()
+        Me.btnRetrieve = New System.Windows.Forms.Button()
+        Me.lblMaleOrFemale = New System.Windows.Forms.Label()
+        Me.txtSecondName = New System.Windows.Forms.TextBox()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtFirstName.Location = New System.Drawing.Point(109, 73)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(112, 20)
+        Me.txtFirstName.TabIndex = 2
+        '
+        'txtCustomerID
+        '
+        Me.txtCustomerID.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtCustomerID.Location = New System.Drawing.Point(109, 47)
+        Me.txtCustomerID.Name = "txtCustomerID"
+        Me.txtCustomerID.Size = New System.Drawing.Size(112, 20)
+        Me.txtCustomerID.TabIndex = 1
+        '
+        'lblCustomerEmail
+        '
+        Me.lblCustomerEmail.AutoSize = True
+        Me.lblCustomerEmail.Location = New System.Drawing.Point(19, 129)
+        Me.lblCustomerEmail.Name = "lblCustomerEmail"
+        Me.lblCustomerEmail.Size = New System.Drawing.Size(79, 13)
+        Me.lblCustomerEmail.TabIndex = 12
+        Me.lblCustomerEmail.Text = "Customer Email"
+        '
+        'lblCustomerName
+        '
+        Me.lblCustomerName.AutoSize = True
+        Me.lblCustomerName.Location = New System.Drawing.Point(19, 76)
+        Me.lblCustomerName.Name = "lblCustomerName"
+        Me.lblCustomerName.Size = New System.Drawing.Size(57, 13)
+        Me.lblCustomerName.TabIndex = 11
+        Me.lblCustomerName.Text = "First Name"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.lblDOB)
+        Me.Panel1.Controls.Add(Me.DateTimePicker1)
+        Me.Panel1.Controls.Add(Me.btnSave)
+        Me.Panel1.Controls.Add(Me.lblName)
+        Me.Panel1.Controls.Add(Me.txtSecondName)
+        Me.Panel1.Controls.Add(Me.txtCustomerEmail)
+        Me.Panel1.Controls.Add(Me.txtFirstName)
+        Me.Panel1.Controls.Add(Me.txtCustomerID)
+        Me.Panel1.Controls.Add(Me.lblMaleOrFemale)
+        Me.Panel1.Controls.Add(Me.lblCustomerEmail)
+        Me.Panel1.Controls.Add(Me.lblCustomerName)
+        Me.Panel1.Controls.Add(Me.lblCustomerID)
+        Me.Panel1.Controls.Add(Me.btnRetrieve)
+        Me.Panel1.Location = New System.Drawing.Point(-10, -12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(305, 284)
+        Me.Panel1.TabIndex = 22
+        '
+        'lblDOB
+        '
+        Me.lblDOB.AutoSize = True
+        Me.lblDOB.Location = New System.Drawing.Point(22, 158)
+        Me.lblDOB.Name = "lblDOB"
+        Me.lblDOB.Size = New System.Drawing.Size(30, 13)
+        Me.lblDOB.TabIndex = 20
+        Me.lblDOB.Text = "DOB"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(109, 158)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(121, 20)
+        Me.DateTimePicker1.TabIndex = 19
+        '
+        'btnSave
+        '
+        Me.btnSave.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btnSave.Location = New System.Drawing.Point(25, 229)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(96, 32)
+        Me.btnSave.TabIndex = 18
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = False
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(4, 15)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(106, 29)
+        Me.lblName.TabIndex = 9
+        Me.lblName.Text = "Holidays"
+        '
+        'txtCustomerEmail
+        '
+        Me.txtCustomerEmail.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtCustomerEmail.Location = New System.Drawing.Point(109, 129)
+        Me.txtCustomerEmail.Name = "txtCustomerEmail"
+        Me.txtCustomerEmail.Size = New System.Drawing.Size(112, 20)
+        Me.txtCustomerEmail.TabIndex = 3
+        '
+        'lblCustomerID
+        '
+        Me.lblCustomerID.AutoSize = True
+        Me.lblCustomerID.Location = New System.Drawing.Point(19, 50)
+        Me.lblCustomerID.Name = "lblCustomerID"
+        Me.lblCustomerID.Size = New System.Drawing.Size(62, 13)
+        Me.lblCustomerID.TabIndex = 10
+        Me.lblCustomerID.Text = "CustomerID"
+        '
+        'btnRetrieve
+        '
+        Me.btnRetrieve.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btnRetrieve.Location = New System.Drawing.Point(149, 229)
+        Me.btnRetrieve.Name = "btnRetrieve"
+        Me.btnRetrieve.Size = New System.Drawing.Size(96, 32)
+        Me.btnRetrieve.TabIndex = 0
+        Me.btnRetrieve.Text = "Retrieve"
+        Me.btnRetrieve.UseVisualStyleBackColor = False
+        '
+        'lblMaleOrFemale
+        '
+        Me.lblMaleOrFemale.AutoSize = True
+        Me.lblMaleOrFemale.Location = New System.Drawing.Point(19, 99)
+        Me.lblMaleOrFemale.Name = "lblMaleOrFemale"
+        Me.lblMaleOrFemale.Size = New System.Drawing.Size(75, 13)
+        Me.lblMaleOrFemale.TabIndex = 13
+        Me.lblMaleOrFemale.Text = "Second Name"
+        '
+        'txtSecondName
+        '
+        Me.txtSecondName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtSecondName.Location = New System.Drawing.Point(109, 99)
+        Me.txtSecondName.Name = "txtSecondName"
+        Me.txtSecondName.Size = New System.Drawing.Size(112, 20)
+        Me.txtSecondName.TabIndex = 4
+        '
+        'Customers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Customers"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents txtCustomerID As System.Windows.Forms.TextBox
+    Private WithEvents lblCustomerEmail As System.Windows.Forms.Label
+    Friend WithEvents lblCustomerName As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents txtCustomerEmail As System.Windows.Forms.TextBox
+    Friend WithEvents lblCustomerID As System.Windows.Forms.Label
+    Friend WithEvents btnRetrieve As System.Windows.Forms.Button
+    Friend WithEvents lblDOB As System.Windows.Forms.Label
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents txtSecondName As System.Windows.Forms.TextBox
+    Friend WithEvents lblMaleOrFemale As System.Windows.Forms.Label
+End Class
---
+++ Mock Exam/Candidate/Holidays/Holidays/Holidays/Customers.vb
@@ -0,0 +1,92 @@
+﻿Imports System.IO
+Public Class Customers
+    Private Structure CustomerDetails
+        Public CustomerID As Integer 'The text entered into the txtCustomerID should be a number 
+        Public FirstName As String
+        Public CustomerEmail As String
+        Public SecondName As String                  'Creating the structure that will hold the data.
+        Public DOB As String
+    End Structure
+
+    Private Sub btnSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSave.Click
+
+        Dim CustomerData As New CustomerDetails
+        Dim sw As New System.IO.StreamWriter("customerdetails.txt", True) 'This is showing the location of where we are writing to.
+        CustomerData.CustomerID = LSet(txtCustomerID.Text, 50) 'This gives a set size to the text that is being entered into the txtFile 
+        CustomerData.FirstName = LSet(txtFirstName.Text, 50)
+        CustomerData.CustomerEmail = LSet(txtSecondName.Text, 50)
+        CustomerData.SecondName = LSet(txtCustomerEmail.Text, 50)   'Filling the structure with data.
+
+
+        If txtCustomerID.Text = "" Then MsgBox("Please fill in all the boxes") 'Making sure that each textbox has text within it 
+        If txtFirstName.Text = "" Then MsgBox("Please fill in all the boxes")
+        If txtSecondName.Text = "" Then MsgBox("Please fill in all the boxes")
+        If txtCustomerEmail.Text = "" Then MsgBox("Please fill in all the boxes")
+
+
+        sw.WriteLine(CustomerData.CustomerID & CustomerData.FirstName & CustomerData.CustomerEmail & CustomerData.SecondName)
+        sw.Close()                                                                  'Always need to close the txtFile we have been writing to 
+        MsgBox("File Saved!") 'The txtFile will be saved before closing
+
+
+
+    End Sub
+
+    Private Sub btnRetrieve_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRetrieve.Click
+        MsgBox("Please enter the CustomerID of the person you are looking for")
+        txtFirstName.Visible = False
+        txtCustomerEmail.Visible = False
+        txtSecondName.Visible = False
+        DateTimePicker1.Visible = False
+
+        Dim CountGot As Integer
+        CountGot = 0
+        Dim CustomerCount As Integer
+        CustomerCount = 0
+
+
+        Dim CustomerData() As String = File.ReadAllLines("customerdetails.txt") 'This Reads every line in the text file.
+        For i = 0 To UBound(CustomerData)
+
+            CountGot = 0
+            If Trim(Mid(CustomerData(i), 1, 50)) = txtCustomerID.Text And Not txtCustomerID.Text = "" Then CountGot = CountGot + 1
+            If Trim(Mid(CustomerData(i), 51, 50)) = txtFirstName.Text And Not txtFirstName.Text = "" Then CountGot = CountGot + 1
+            If Trim(Mid(CustomerData(i), 101, 50)) = txtCustomerEmail.Text And Not txtCustomerEmail.Text = "" Then CountGot = CountGot + 1
+            If Trim(Mid(CustomerData(i), 151, 50)) = txtSecondName.Text And Not txtSecondName.Text = "" Then CountGot = CountGot + 1 'Counting how many attributes follow the search
+
+            If CountGot > 0 Then CustomerCount = CustomerCount + 1 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''If attributes match, add to the count.
+        Next i
+        MsgBox("There is : " & CustomerCount & " Customer Found") 'This lets you know if a certain customers details were found 
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        If Dir$("customerdetails.txt") = "" Then
+            Dim sw As New StreamWriter("customerdetails.txt", True)    'This makes sure there is actually a database to enter/read data. If not, it creates a new blank one.
+            sw.WriteLine("")
+            sw.Close() 'This closes the SteamWriter
+            MsgBox("A new file has been created", vbExclamation, "Warning!") 'This tells you that a new text file has been created.
+        End If
+    End Sub
+
+    Private Sub txtCustomerID_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtCustomerID.KeyPress
+        If Not IsNumeric(e.KeyChar) Then
+            MsgBox("This is not a number")
+            e.KeyChar = Nothing     'This does not allow a character to be entered into this textbox 
+        End If
+    End Sub
+
+    Private Sub txtCustomerName_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtFirstName.KeyPress
+        If IsNumeric(e.KeyChar) Then
+            MsgBox("You cannot enter a number into here")
+            e.KeyChar = Nothing     'This does not allow a number to be entered into this textbox 
+        End If
+    End Sub
+
+    Private Sub txtSecondName_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtSecondName.KeyPress, txtSecondName.KeyPress
+
+        If IsNumeric(e.KeyChar) Then
+            MsgBox("You cannot enter a number into here")
+            e.KeyChar = Nothing
+        End If
+    End Sub
+End Class
